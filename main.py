ac9d583977e2e06b342c7f72eb88b4fd2d36a185
sshlpp <arsenii.tsesko@gmail.com>
2025-05-26 15:26:17 +0200

    + update funcs

--- main.py
+++ main.py
@@ -106,7 +106,6 @@ def create_task() -> None:
     }
     add_task(tasks[task_id])
     
-
 def print_tasks():
     file = open("tasks.txt", "r")
     while True:    
@@ -122,15 +121,151 @@ def print_tasks():
 
             case "0":
                 break
-
-
-
     file.close()
 
+def update_title() -> None:
+    id = input("Enter task ID: ")
+    task_list = []
+    with open("tasks.txt", "r") as file:
+        for line in file:
+            task_list.append(line.split(" | "))
+    new_title = input("Enter new title: ")
 
-# def update_task():
+    for task in task_list:
+        if task[0] == id:
+            task[1] = new_title
 
-# def del_task():
+    with open("tasks.txt", "w") as file:
+        for task in task_list:
+            file.write(" | ".join(task))
+
+    global tasks
+    tasks = {int(task[0]): {
+        "ID": int(task[0]),
+        "Title": task[1],
+        "Description": task[2],
+        "Priority": task[3],
+        "Status": task[4]
+    } for task in task_list}
+
+def update_desc() -> None:
+    id = input("Enter task ID: ")
+    task_list = []
+    with open("tasks.txt", "r") as file:
+        for line in file:
+            task_list.append(line.split(" | "))
+    new_desc = input("Enter new title: ")
+
+    for task in task_list:
+        if task[0] == id:
+            task[2] = new_desc
+
+    with open("tasks.txt", "w") as file:
+        for task in task_list:
+            file.write(" | ".join(task))
+
+    global tasks
+    tasks = {int(task[0]): {
+        "ID": int(task[0]),
+        "Title": task[1],
+        "Description": task[2],
+        "Priority": task[3],
+        "Status": task[4]
+    } for task in task_list}
+
+def update_status() -> None:
+    id = input("Enter task ID: ")
+    task_list = []
+    with open("tasks.txt", "r") as file:
+        for line in file:
+            task_list.append(line.split(" | "))
+    new_status = check_status()
+
+    for task in task_list:
+        if task[0] == id:
+            task[4] = f"{new_status}\n"
+
+    with open("tasks.txt", "w") as file:
+        for task in task_list:
+            file.write(" | ".join(task))
+
+    global tasks
+    tasks = {int(task[0]): {
+        "ID": int(task[0]),
+        "Title": task[1],
+        "Description": task[2],
+        "Priority": task[3],
+        "Status": task[4]
+    } for task in task_list}
+
+def update_priority() -> None:
+    id = input("Enter task ID: ")
+    task_list = []
+    with open("tasks.txt", "r") as file:
+        for line in file:
+            task_list.append(line.split(" | "))
+    new_priority = check_priority()
+
+    for task in task_list:
+        if task[0] == id:
+            task[3] = new_priority
+
+    with open("tasks.txt", "w") as file:
+        for task in task_list:
+            file.write(" | ".join(task))
+
+    global tasks
+    tasks = {int(task[0]): {
+        "ID": int(task[0]),
+        "Title": task[1],
+        "Description": task[2],
+        "Priority": task[3],
+        "Status": task[4]
+    } for task in task_list}
+
+def update_task():
+    while True:
+        choise = input("1 - название\n2 - описание\n3 - приоритет\n4 - статус\n0 - Вернуться назад\n")
+        match choise:
+            case "1":
+                update_title()
+            case "2":
+                update_desc()
+            case "3":
+                update_priority()
+            case "4":
+                update_status()
+            case "0":
+                break
+        
+
+def del_task() -> None:
+    id = input("Enter task ID: ")
+    task_list = []
+    with open("tasks.txt", "r") as file:
+        for line in file:
+            task_list.append(line.split(" | "))
+    
+    updated_list = [task for task in task_list if task[0] != id]
+
+    if len(updated_list) == len(task_list):
+        print("ID not found")
+        return 
+
+    with open("tasks.txt", "w") as file:
+        for task in updated_list:
+            file.write(" | ".join(task))
+
+    global tasks
+    tasks = {task[0]: {
+        "ID": task[0],
+        "Title": task[1],
+        "Description": task[2],
+        "Priority": task[3],
+        "Status": task[4]
+    } for task in updated_list}
+
+
 
 
 def main():
@@ -141,10 +276,10 @@ def main():
                 create_task()
             case "2":
                 print_tasks()
-            # case "3":
-            #     update_task():
-            # case "4":
-            #     del_task():
+            case "3":
+                update_task()
+            case "4":
+                del_task()
             case "0":
                 break
 
